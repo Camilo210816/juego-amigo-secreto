c056fb9a829ad34d1774bd1cbb606ea5abd61361
Camilo210816 <camilo210816@gmail.com>
2025-08-13 00:06:52 -0500

    Actualizacion app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos=[]
+function agregarAmigo(){
+    nombreAmigo=document.getElementById("amigo").value
+    if (nombreAmigo==""){
+        alert("Por favor ingresa un nombre válido")
+    } else {
+        listaAmigos.push(nombreAmigo)
+        document.getElementById("amigo").value=""
+    }
+}
 
+function sortearAmigo(){}
