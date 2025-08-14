1c5a6958293e7d323eb3dd9fdfde7f2b2200f181
Camilo210816 <camilo210816@gmail.com>
2025-08-14 00:04:22 -0500

    Final version 1

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos=[]
+// función que genera la lista de amigos ingresada por el usuario.
 function agregarAmigo(){
-    nombreAmigo=document.getElementById("amigo").value
+    nombreAmigo=document.getElementById("amigo").value;
+    // Aquí se ingresan los nombres a la lista, siempre que sean válidos.
     if (nombreAmigo==""){
-        alert("Por favor ingresa un nombre válido")
+        alert("Por favor ingresa un nombre válido");
     } else {
-        listaAmigos.push(nombreAmigo)
-        document.getElementById("amigo").value=""
+        listaAmigos.push(nombreAmigo);
+        document.getElementById("amigo").value="";
+        actualizarLista();
     }
 }
 
-function sortearAmigo(){}
+// función que muestra la lista en pantalla de los amigos que ha ingresado el usuario.
+function actualizarLista(){
+    lista=document.getElementById("listaAmigos");
+    lista.innerHTML="";
+    // Aquí se muestra la lista al usuario
+    for (let i=0;i<listaAmigos.length;i+=1){
+        lista.innerHTML+=`<li> ${listaAmigos[i]} </li>`;
+    }
+}
+//función para el botón de sortear amigo, realiza el sorteo del amigo secreto.
+function sortearAmigo(){
+    if (listaAmigos.length>0){
+        indice=Math.floor(Math.random()*(listaAmigos.length));
+        amigoSorteado=listaAmigos[indice];
+        document.getElementById("resultado").innerHTML=`Tu amigo secreto es ${amigoSorteado}`;
+        console.log(indice)
+    }
+}
